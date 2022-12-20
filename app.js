b0c770de7579a11040c88200c3b48aacb71bd91e
manish7922 <100351106+manish7922@users.noreply.github.com>
2022-12-20 11:12:21 +0530

    update data

--- app.js
+++ app.js
@@ -55,7 +55,9 @@ app.get("/products", function (req, res) {
 });
 app.get("/purchases", function (req, res) {
   let sort = req.query.sort;
-  let values = [sort];
+  let productid = req.query.productid;
+  let shop = req.query.shop;
+
   console.log("Inside /users get api");
   const query = "SELECT * FROM purchases";
   client.query(query, function (err, result) {
@@ -63,22 +65,32 @@ app.get("/purchases", function (req, res) {
       res.status(400).send(err);
     }
     let results = result.rows;
-    // if (sort === "QtyAsc")
-    //   results = results.sort((st1, st2) => st1.quantity - st2.quantity);
-    // if (sort === "QtyDesc")
-    //   results = results.sort((st1, st2) => st2.quantity - st1.quantity);
-    // if (sort === "ValueAsc")
-    //   results = results.sort(
-    //     (st1, st2) => st1.price * st1.quantity - st2.price * st2.quantity
-    //   );
-    // if (sort === "ValueDesc")
-    //   results = results.sort(
-    //     (st1, st2) => st2.price * st2.quantity - st1.price * st1.quantity
-    //   );
-    res.send(result.rows);
-    console.log(result);
+    console.log(results);
+
+    results = filterParam(results, "productid", productid);
+
+    if (shop) {
+      results = results.filter((n) => n.shopid == shop);
+      console.log(results);
+    }
+    // if (product) {
+    //   results = results.filter((n) => n.productid == product);
+    // }
+    res.send(results);
+    console.log(results);
   });
 });
+const filterParam = (arr, name, values) => {
+  console.log(name);
+  if (!values) return arr;
+  let valuesArr = values.split(",");
+  console.log(valuesArr);
+  let arr1 = arr.filter((a1) => valuesArr.find((val) => val == a1[name]));
+  console.log(arr1);
+  // console.log(name);
+  // console.log(arr);
+  return arr1;
+};
 app.post("/shops", function (req, res) {
   var values = Object.values(req.body);
 
@@ -107,9 +119,9 @@ app.post("/products", function (req, res) {
     res.send(`${result.rows} insertion successful`);
   });
 });
-app.get("/products/:id", function (req, res) {
-  let id = +req.params.id;
-  let values = [id];
+app.get("/products/:productid", function (req, res) {
+  let productid = +req.params.productid;
+  let values = [productid];
 
   console.log("Inside /users get api with id");
   const query = ` SELECT * FROM products WHERE productid=$1`;
@@ -119,14 +131,14 @@ app.get("/products/:id", function (req, res) {
     }
 
     let results = result.rows;
-    let arr = results.find((n) => n.productid === id);
+    let arr = results.find((n) => n.productid === productid);
     console.log(arr);
     if (arr) res.send(arr);
     else res.status(404).send("no student find");
   });
 });
 app.put("/products/:productid", function (req, res, next) {
-  let productid = req.params.productid;
+  let productid = +req.params.productid;
   let productname = req.body.productname;
   let category = req.body.category;
   let description = req.body.description;
@@ -134,7 +146,7 @@ app.put("/products/:productid", function (req, res, next) {
 
   console.log("Inside /users edit");
 
-  const query = `UPDATE products SETproductname=$1 category=$2, description=$3 WHERE productid=$4`;
+  const query = `UPDATE products SET productname=$1, category=$2, description=$3 WHERE productid=$4`;
   client.query(query, values, function (err, result) {
     if (err) {
       res.status(400).send(err);
@@ -199,9 +211,7 @@ app.get("/purchases/product/:productid", function (req, res) {
 
 app.post("/purchases", function (req, res) {
   var values = Object.values(req.body);
-
   console.log("Inside /users get api with brand");
-
   const query = `INSERT INTO purchases(shopId, productId,quantity,price) VALUES ($1,$2,$3,$4)`;
   client.query(query, values, function (err, result) {
     if (err) {
@@ -209,5 +219,41 @@ app.post("/purchases", function (req, res) {
     }
     console.log(result);
     res.send(`${result.rows} insertion successful`);
+  });
+});
+app.get("/totalPurchase/shop/:id", function (req, res) {
+  let shopid = req.params.id;
+  // let productid = req.params.productid;
+  console.log(shopid);
+  let values = [shopid];
+  console.log("Inside /users get api with brand");
+  const query = `SELECT productid, SUM(quantity) as totalSum FROM purchases where shopid=$1 GROUP BY productid`;
+
+  client.query(query, values, function (err, result) {
+    if (err) {
+      res.status(400).send(err);
+    }
+    let results = result.rows;
+    // let data = JSON.parse(results);
+    // let newData = { ID: data.productid, sum: data.totalsum };
+    // let data1 = JSON.stringify(data);
+    console.log(results);
+    res.json(results);
+  });
+});
+app.get("/totalPurchase/product/:id", function (req, res) {
+  let productid = req.params.id;
+  console.log(productid);
+  let values = [productid];
+  console.log("Inside /users get api with brand");
+  const query = `SELECT shopid, SUM(quantity) as totalSum FROM purchases where productid=$1 GROUP BY shopid`;
+  client.query(query, values, function (err, result) {
+    if (err) {
+      res.status(400).send(err);
+    }
+    let results = result.rows;
+
+    console.log(results);
+    res.json(results);
   });
 });
